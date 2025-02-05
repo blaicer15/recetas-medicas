46ba9bdc09e6b11c76c59d8af3d95516cd0b80a0
blaicer15 <blaicer15@gmail.com>
2025-02-05 20:10:15 +0000

    feat (main route): format code

--- lib/router/main.route.dart
+++ lib/router/main.route.dart
@@ -4,5 +4,8 @@ import 'package:recetas_medicas/view/login.dart';
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
   GoRoute(path: '/', builder: (context, state) => const LoginView()),
-  GoRoute(path: '/home', builder: (context, state) => const HomeView())
+  GoRoute(
+      path: '/home',
+      name: "home",
+      builder: (context, state) => const HomeView())
 ]);
