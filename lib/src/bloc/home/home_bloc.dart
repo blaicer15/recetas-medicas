881710dc5db0facaf04b674c7552cfe7afb1bdf0
Jorge de Jesus Espinoza Martinez <jdjezmp@gmail.com>
2025-02-06 10:37:23 -0600

    fix problems

--- lib/src/bloc/home/home_bloc.dart
+++ lib/src/bloc/home/home_bloc.dart
@@ -1,14 +1,15 @@
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 
 part 'home_event.dart';
 part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   HomeBloc() : super(const HomeState()) {
-    on<HomeTabSelected>(_onChangeTab);
+    on<HomeTabChanged>(_onChangeTab);
   }
 
-  void _onChangeTab(HomeTabSelected event, Emitter<HomeState> emit) {
+  void _onChangeTab(HomeTabChanged event, Emitter<HomeState> emit) {
     emit(state.copyWith(tab: event.tab));
   }
 }
