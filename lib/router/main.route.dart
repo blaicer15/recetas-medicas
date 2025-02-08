b74da55503de08e322c4b71fa591b71d6e8087f5
Jorge de Jesus Espinoza Martinez <jdjezmp@gmail.com>
2025-02-08 13:45:55 -0600

    feat (main route): nueva view

--- lib/router/main.route.dart
+++ lib/router/main.route.dart
@@ -1,9 +1,14 @@
 import 'package:go_router/go_router.dart';
+import 'package:recetas/view/Splash.dart';
 import 'package:recetas/view/home.dart';
 import 'package:recetas/view/login.dart';
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
-  GoRoute(path: '/', builder: (context, state) => const LoginView()),
+  GoRoute(
+    path: '/',
+    builder: (context, state) => const SplashView(),
+  ),
+  GoRoute(path: '/login', builder: (context, state) => const LoginView()),
   GoRoute(
       path: '/home',
       name: "home",
