1584720ee361049bedb7a6603f27f884e2843588
Jorge de Jesus Espinoza Martinez <jdjezmp@gmail.com>
2025-02-10 09:51:54 -0600

    feat (home view):import components

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:recetas/src/components/home/personas.dart';
+import 'package:recetas/src/components/home/recetas.dart';
 import 'package:recetas/src/components/navigation_bar.dart';
 import 'package:recetas/src/cubit/navigation/navigation_cubit.dart';
 
@@ -18,13 +20,9 @@ class HomeView extends StatelessWidget {
               builder: (context, state) {
             switch (state.tab) {
               case 0:
-                return const Center(
-                  child: Text('Recetas'),
-                );
+                return const Recetas();
               case 1:
-                return const Center(
-                  child: Text('Personas'),
-                );
+                return const Personas();
               default:
                 return const Center(
                   child: Text('No seleccionado'),
@@ -32,7 +30,10 @@ class HomeView extends StatelessWidget {
             }
           }),
         ),
-        bottomNavigationBar: const BarNavigation(),
+        bottomNavigationBar: BlocProvider(
+          create: (context) => NavigationBarCubit(),
+          child: const BarNavigation(),
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             // Acción del botón flotante
