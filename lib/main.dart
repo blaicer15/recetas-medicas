01a4761a85f6e69e7bc72e86bf3f75bcefae5ae1
Jorge de Jesus Espinoza Martinez <jdjezmp@gmail.com>
2025-02-11 14:15:00 -0600

    feat (main): configuraciones adicionales
    - Configurando env
    - Bloc Provider auth para current user

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:recetas/router/main.route.dart';
+import 'package:recetas/src/cubit/auth/auth_cubit.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-const supabaseUrl = 'https://fqeulcltjcbymahuvtml.supabase.co';
-
 Future<void> main() async {
-  await Supabase.initialize(url: supabaseUrl, anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxZXVsY2x0amNieW1haHV2dG1sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU1NzU5MDUsImV4cCI6MjA1MTE1MTkwNX0.mfo1uy2DCz-S5aOcgc4gf5yUBGND1mJM92GifgBq6Hs',authOptions: const FlutterAuthClientOptions(
-    authFlowType: AuthFlowType.pkce,
-  ),
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
+  await Supabase.initialize(
+      url: dotenv.get("SUPABASE_URL"),
+      anonKey: dotenv.get("SUPABASE_ANON_KEY"),
+      authOptions: const FlutterAuthClientOptions(
+        authFlowType: AuthFlowType.pkce,
+      ),
       realtimeClientOptions: const RealtimeClientOptions(
         logLevel: RealtimeLogLevel.info,
       ),
@@ -23,13 +31,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      title: "Recetas medicas",
-      routerConfig: appRouter,
-      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple), useMaterial3: true),
+    return BlocProvider(
+      create: (context) => AuthCubit(),
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        title: "Recetas medicas",
+        routerConfig: appRouter,
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true),
+      ),
     );
   }
-
-
 }
